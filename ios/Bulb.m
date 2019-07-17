9bd030f4f901926dec7a4777739c712edc37b5fa
Umair <umair170@hotmail.com>
2019-07-17 13:47:54 +0100

    final demo

---
+++ ios/Bulb.m
@@ -0,0 +1,7 @@
+#import "React/RCTBridgeModule.h"
+
+@interface RCT_EXTERN_MODULE(Bulb, NSObject)
+RCT_EXTERN_METHOD(turnOn)
+RCT_EXTERN_METHOD(turnOff)
+RCT_EXTERN_METHOD(getStatus: (RCTResponseSenderBlock)callback)
+@end
